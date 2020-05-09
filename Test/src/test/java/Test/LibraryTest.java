68382495ba8d675910c9c57dc122d7e1b7f3dbe3
qa-atish <53812864+qa-atish@users.noreply.github.com>
2020-05-09 08:42:23 +0530

    Update LibraryTest.java

--- Test/src/test/java/Test/LibraryTest.java
+++ Test/src/test/java/Test/LibraryTest.java
@@ -1,5 +1,7 @@
 package Test;
 
+import static org.junit.Assert.assertTrue;
+
 import org.testng.annotations.Test;
 
 import com.sentieo.baseClass.BaseClass;
@@ -15,10 +17,13 @@ public class LibraryTest extends BaseClass {
 		driver.get(appUrl);
 		Thread.sleep(3000);
 		page = new PageObejcts(driver);
-		boolean getValue = page.getEmailCount("abc@gmail.com", "abc@carrothr.com", 2);
+		boolean getValue = page.getEmailCount("abc@gmail.com", "abc@carrothr.com", 1);
 		if (getValue) {
 			page = new PageObejcts(driver);
-			page.verifyTheSecondPage("Check your email");
+			boolean status = page.verifyTheSecondPage("Check your email");
+			if (!status) {
+				assertTrue("Second Page not visible", false);
+			}
 		}
 	}
 
@@ -28,11 +33,11 @@ public class LibraryTest extends BaseClass {
 		page = new PageObejcts(driver);
 		boolean status = page.selectTheButton("Features", "Anniversaries & Birthdays");
 		if (!status) {
-			System.out.println("Anniversaries & Birthdays tab is not active yet");
+			assertTrue("Anniversaries & Birthdays tab is not active yet", false);
 		}
 	}
 
-	@Test(groups = "sanity,regression,smoke", description = "verify the complete flow of ", priority = 2)
+	@Test(groups = "sanity,regression,smoke", description = "verify the complete flow of contact us ", priority = 2)
 	public void contactUs() throws Exception {
 
 		page = new PageObejcts(driver);
@@ -43,10 +48,10 @@ public class LibraryTest extends BaseClass {
 		if (status) {
 			boolean suggestionVisible = page.getSuggestions();
 			if (!suggestionVisible) {
-				System.out.println("Suggestion not visible");
+				assertTrue("Suggestion not visible", false);
 			}
 		} else {
-			System.out.println("Intercom chat box not opened");
+			assertTrue("Intercom is not opened", false);
 		}
 	}
 }
