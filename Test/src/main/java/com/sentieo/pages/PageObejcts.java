899794282e36686ea9e9694790eab829cbfc48fe
qa-atish <53812864+qa-atish@users.noreply.github.com>
2020-09-12 22:31:14 +0530

    Update PageObejcts.java

--- Test/src/main/java/com/sentieo/pages/PageObejcts.java
+++ Test/src/main/java/com/sentieo/pages/PageObejcts.java
@@ -1,145 +1,106 @@
 package com.sentieo.pages;
 
-import static org.junit.Assert.assertTrue;
-import java.util.List;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class PageObejcts {
+import com.sentieo.baseClass.LoggingUtils;
+
+public class NewPageObject {
 
 	WebDriver driver;
 
-	@FindBy(css = ".input-group")
-	List<WebElement> emailBox;
+	@FindBy(name = "reg.email")
+	WebElement _register_enterEmail;
 
-	@FindBy(css = ".form-inline .btn-tryFree")
-	WebElement tryforFreeButton;
+	@FindBy(name = "reg.password")
+	WebElement _register_enterPassword;
 
-	@FindBy(css = ".readmin-page-content .jss12 .MuiTypography-root")
-	List<WebElement> nextPageVerify;
+	@FindBy(name = "reg.confirmPassword")
+	WebElement _register_confirmPassword;
 
-	@FindBy(css = ".container .topbar-left .nav-item")
-	List<WebElement> featureButtons;
+	@FindBy(name = "_action_doRegister")
+	WebElement _register_submitButton;
 
-	@FindBy(css = ".row.gap-5 .nav .nav-item .nav-link")
-	List<WebElement> selectAnniversary;
+	@FindBy(xpath = "/html/body/div[2]")
+	WebElement _getNextPageText;
 
-	@FindBy(css = ".site-footer .container .launch_intercom")
-	WebElement contactUsButton;
+	@FindBy(name = "fp.email")
+	WebElement _forgetPassword_enterEmail;
 
-	@FindBy(css = ".intercom-messenger-frame")
-	WebElement intercomView;
+	@FindBy(id = "forgotPasswordButton")
+	WebElement _forgetPassword_SubmitButton;
 
-	@FindBy(css = ".intercom-1xafcqx .intercom-messenger-card-body .intercom-1naovro")
-	WebElement intercomMsgSubmitButton;
+	@FindBy(id = "fpSuccessModal")
+	WebElement _forgetPassword_confirmPopup;
 
-	@FindBy(css = ".intercom-1xafcqx .intercom-messenger-card-body .intercom-1naovro")
-	WebElement intercomMsgTextArea;
+	@FindBy(css = ".modal-dialog .modal-footer .btn-primary")
+	WebElement _forgetPassword_closePopup;
 
-	@FindBy(css = ".intercom-1xafcqx .intercom-messenger-card-body .intercom-messenger-card-component .intercom-1xftouw .intercom-messenger-card-list-item")
-	List<WebElement> intercomMsgSuggestion;
+	@FindBy(name = "creds.username")
+	WebElement _login_userName;
 
-	public PageObejcts(WebDriver driver) {
+	@FindBy(name = "creds.password")
+	WebElement _login_password;
+
+	@FindBy(id = "loginButton")
+	WebElement _login_LoginButton;
+
+	public NewPageObject(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
 
-	public boolean getEmailCount(String wrongEmail, String validEmail, int number) throws InterruptedException {
+	public void registerUser(String userName, String password) throws InterruptedException {
 
 		Actions actions = new Actions(driver);
-		if (emailBox.size() > number) {
-			System.out.println("Total number of Email text box : " + emailBox.size());
-			for (int i = 0; i < emailBox.size(); i++) {
-				if (i == (number)) {
-					WebElement ele = emailBox.get(i).findElement(By.cssSelector(".fs-16.form-control-new"));
-					((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", ele);
-					actions.moveToElement(ele).click().sendKeys(wrongEmail).build().perform();
-					actions.moveToElement(tryforFreeButton).click().build().perform();
-					Thread.sleep(3000);
-					String getPlaceHolder = ele.getAttribute("placeholder");
-					if (getPlaceHolder.equals("Please try a work email")) {
-						actions.moveToElement(ele).click().sendKeys(validEmail).build().perform();
-						actions.moveToElement(tryforFreeButton).click().build().perform();
-						Thread.sleep(3000);
-						return true;
-					}
-				}
-			}
+		LoggingUtils.info("Enter UserName " + (userName));
+		actions.moveToElement(_register_enterEmail).click().sendKeys(userName).build().perform();
+		LoggingUtils.info("Enter password " + (password));
+		actions.moveToElement(_register_enterPassword).click().sendKeys(password).build().perform();
+		LoggingUtils.info("Enter confirm password " + (password));
+		actions.moveToElement(_register_confirmPassword).click().sendKeys(password).build().perform();
+		LoggingUtils.info("Submit the details ");
+		actions.moveToElement(_register_submitButton).click().build().perform();
+		new NewPageObject(driver);
+		String getText = _getNextPageText.getText();
+		if (getText.equalsIgnoreCase("Check your email for account verification instructions.")) {
+			LoggingUtils.info("User registered successfully ");
 		}
-		return false;
 	}
 
-	public boolean verifyTheSecondPage(String text) throws InterruptedException {
-		for (int i = 0; i < nextPageVerify.size(); i++) {
-			String getText = nextPageVerify.get(i).getText();
-			if (getText.equals(text)) {
-				driver.navigate().back();
-				Thread.sleep(2000);
-				return false;
-			}
-		}
-		return false;
-	}
-
-	public boolean selectTheButton(String text, String ann) throws InterruptedException {
-		for (int i = 0; i < featureButtons.size(); i++) {
-			String getText = featureButtons.get(i).getText();
-			if (getText.equals(text)) {
-				featureButtons.get(i).click();
-				for (int j = 0; j < selectAnniversary.size(); j++) {
-					String getName = selectAnniversary.get(j).getText();
-					String classState = selectAnniversary.get(j).getAttribute("class");
-					if (getName.contains("Recognition")) {
-						if (!classState.contains("active")) {
-							assertTrue("By default recongnition is selected", false);
-						}
-					} else if (getName.contains(ann)) {
-						selectAnniversary.get(j).click();
-						Thread.sleep(2000);
-						String getState = selectAnniversary.get(j).getAttribute("class");
-						if (getState.contains("active")) {
-							return true;
-						}
-					}
-				}
-			}
-		}
-		return false;
-	}
-
-	public void contactUS() throws InterruptedException {
+	public void password_User(String userName) throws InterruptedException {
 
 		Actions actions = new Actions(driver);
-		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", contactUsButton);
-		actions.moveToElement(contactUsButton).click().build().perform();
-		Thread.sleep(2000);
-	}
-
-	public boolean getSuggestions() {
-
-		if (intercomMsgSuggestion.size() > 0) {
-			return true;
+		LoggingUtils.info("Enter email address for password reset :" + (userName));
+		actions.moveToElement(_forgetPassword_enterEmail).click().sendKeys(userName).build().perform();
+		LoggingUtils.info("Submit the details ");
+		actions.moveToElement(_forgetPassword_SubmitButton).click().build().perform();
+		Thread.sleep(1000);
+		new NewPageObject(driver);
+		String getText = _forgetPassword_confirmPopup.getAttribute("style");
+		if (getText.contains("display: block;")) {
+			LoggingUtils.info("Close the confirmation popup :");
+			actions.moveToElement(_forgetPassword_closePopup).click().build().perform();
 		}
-		return false;
 	}
 
-	public boolean setTextIntercom(String text) throws InterruptedException {
+	public void complete_Successful(String userName, String password) throws InterruptedException {
+
 		Actions actions = new Actions(driver);
-		if (intercomView.isDisplayed()) {
-			driver.switchTo().frame("intercom-messenger-frame");
-			JavascriptExecutor java = (JavascriptExecutor) driver;
-			WebElement element = (WebElement) java.executeScript(
-					"return  document.querySelectorAll('.intercom-1qgdrjz.ea4em4b0 div')[0].children[0]");
-			actions.moveToElement(element).click().sendKeys(text).build().perform();
-			actions.moveToElement(intercomMsgSubmitButton).click().build().perform();
-			Thread.sleep(3000);
-			return true;
+		LoggingUtils.info("Enter UserName " + (userName));
+		actions.moveToElement(_login_userName).click().sendKeys(userName).build().perform();
+		LoggingUtils.info("Enter password " + (password));
+		actions.moveToElement(_login_password).click().sendKeys(password).build().perform();
+		LoggingUtils.info("Submit the details ");
+		actions.moveToElement(_login_LoginButton).click().build().perform();
+		Thread.sleep(1000);
+		new NewPageObject(driver);
+		String getText = _getNextPageText.getText();
+		if (getText.equalsIgnoreCase("You have successfully logged in.")) {
+			LoggingUtils.info("User login successfully ");
 		}
-		return false;
 	}
 }
